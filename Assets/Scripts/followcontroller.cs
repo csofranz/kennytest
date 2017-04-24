a2a00aee99301da33160378bf58feda8c55718d8
Christian Franz <csofranz@icloud.com>
2017-04-24 17:29:19 +0200

    add trail for controller
    ugly. but works. demo for trails that show movement of controllers

---
+++ Assets/Scripts/followcontroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followcontroller : MonoBehaviour {
+
+	//
+	// script that makes this object follow the leader
+	// you can config offset x, y, z
+	//
+	public GameObject theLeader; // we'll follow this object around
+	public float offset;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// ok, set my position to that of the leader
+		Vector3 thePos = theLeader.transform.position;
+		Quaternion theRot = theLeader.transform.rotation;
+		GameObject self = gameObject;
+		self.transform.position = thePos;
+		self.transform.rotation = theRot;
+
+		self.transform.Translate (Vector3.forward * offset);
+
+	}
+}
